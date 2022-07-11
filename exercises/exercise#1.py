1bc2e9a627865576faa8e1145e023f5a28165339
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-07-11 20:47:51 +0430

    Jacobify - new exercises

---
+++ exercises/exercise#1.py
@@ -0,0 +1,12 @@
+# Calculation of squares for:
+# 6x6 quilt
+
+# 7x7 quilt
+
+# 8x8 quilt
+
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+
+print( 6 **4  )
+#print(7 **2 )
+#print(8 **2 )
---
+++ exercises/exercise#2.py
@@ -0,0 +1,59 @@
+# modulo method
+
+my_team = 28 % 4
+your_team = 27 % 4
+
+print(my_team)
+print(your_team)
+
+
+# ------------------------------------------------ concatation
+
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+print(message)
+
+my_age = 25
+half_my_age = 12.5
+
+greeting = "well hello there"
+
+name = "Farzad"
+
+greeting_with_name = greeting + name
+# ------------------------------------------------ (( += ))
+
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+
+total_price += nice_sweater
+total_price += fun_books
+# Update total_price here:
+
+print("The total price is", total_price)
+
+# ------------------------------------------------- multi line string
+to_you = """ Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you? """
+
+
+print(to_you)
+
+# -------------------------------------------------
+
