fbdc5cab138cd1c1d8f832f2502f3684d599573d
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-07-23 10:55:03 +0430

    Jacobify - recent updates

--- Project/draft#1.py
+++ Project/draft#1.py
@@ -1,3 +1,27 @@
-first_name = " SSS\nS   S\nS\n SSS\n    S\nS   S\n SSS"
+first_name = " SSS\nS\rS\nS\n SSS\n    S\nS\rS\n SSS"
 
+#print(first_name)
+
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * SSS               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
+
+first_name = "FFFFFFFF\nF\nF\nFFFFF\nF\nF\nF"
+last_name = "Y\tY\n  Y   Y\n    Y\n    Y\n    Y\n    Y\n    Y"
 print(first_name)
+print(last_name)
---
+++ exercises/condiontinals.py
@@ -0,0 +1,28 @@
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+
+weight = 185
+# planet = 6
+
+# Write an if statement below:
+
+planet = int(input("Please Enter a number for desired planet"))
+
+
+
+if planet == 1:
+  weight *= 0.91
+elif planet == 2:
+  weight *= 0.38
+elif planet == 3:
+  weight *= 2.34
+elif planet == 4:
+  weight *= 1.06
+elif planet == 5:
+  weight *= 0.92
+elif planet == 6:
+  weight *= 1.19
+
+print("your weight would be ",weight)
