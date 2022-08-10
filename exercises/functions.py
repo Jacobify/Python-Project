332efb8dab99b02d4b7c356a3d27a4fa1e266a2c
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-08-10 12:56:00 +0430

    Jacobify - functions update

---
+++ exercises/functions.py
@@ -0,0 +1,94 @@
+# Your code below:
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station")
+  print("Take the Northbound N, Q, R, or W train 1 stop")
+  print("Get off the Times Square 42nd Street stop")
+
+
+# Your code below:
+def generate_trip_instructions(location):
+  print("Looks like you are planning a trip to visit",location)
+  print("You can use the public subway system to get to",location)
+
+generate_trip_instructions("Grand Central Station")
+
+# Write your code below:
+def calculate_expenses(plane_ticket_price,car_rental_rate,hotel_rate,trip_time):
+  car_rental_total = car_rental_rate * trip_time
+  hotel_total = hotel_rate * trip_time - 10
+  print(car_rental_total+hotel_total +plane_ticket_price)
+
+calculate_expenses(200,100,100,5)
+
+# Write your code below:
+
+def trip_planner(first_destination, second_destination, final_destination="Codecademy HQ"):
+  print("Here is what your trip will look like!")
+  print("First, we will stop in " + first_destination + ", then " + second_destination + ", and lastly " + final_destination)
+
+trip_planner("France", "Germany", "Denmark")
+trip_planner("Denmark", "France", "Germany")
+trip_planner(first_destination="Iceland", final_destination="Germany", second_destination="India")
+trip_planner("Brooklyn", "Queens")
+
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+max_price = max(tshirt_price,
+shorts_price,
+mug_price,
+poster_price)
+min_price = min(tshirt_price,
+shorts_price,
+mug_price,
+poster_price)
+rounded_price = round(tshirt_price,1)
+print(max_price)
+print(min_price)
+print(rounded_price)
+
+
+# This function will print a hardcoded count of how many locations we have.
+favorite_locations = "Paris, Norway, Iceland"
+
+def print_count_locations():
+  print("There are 3 locations")
+
+# This function will print the favorite locations
+def show_favorite_locations():
+  print("Your favorite locations are: " + favorite_locations)
+
+print_count_locations()
+show_favorite_locations()
+
+current_budget = 3500.75
+
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+
+print_remaining_budget(current_budget)
+
+# Write your code below:
+shirt_expense =9
+def deduct_expense(budget,expense):
+  return budget - expense
+
+new_budget_after_shirt =  deduct_expense(current_budget,shirt_expense)
+
+print_remaining_budget(new_budget_after_shirt)
+# -------------------------------------------------
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first,second,third
+
+most_popular1,most_popular2,most_popular3 =  top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
