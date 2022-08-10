837aeb28fbd78e6c80558b1c184c006ebfdebff0
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-08-10 14:18:40 +0430

    Jacobify - function exercises finished

--- exercises/functions.py
+++ exercises/functions.py
@@ -92,3 +92,22 @@ most_popular1,most_popular2,most_popular3 =  top_tourist_locations_italy()
 print(most_popular1)
 print(most_popular2)
 print(most_popular3)
+# -----------------------------------------------
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0",name)
+
+trip_planner_welcome("Farzad")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(24.6)
+
+def destination_setup(origin,destination,estimated_time,mode_of_transport="Car"):
+  print("Your trip starts off in",origin)
+  print("And you are traveling to",destination)
+  print("You will be traveling by",mode_of_transport)
+  print("It will take approximately",estimated_time,"hours")
+
+destination_setup("Mashhad","Vida",estimate)
