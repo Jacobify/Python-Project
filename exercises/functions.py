565393ec4f10621a4494a5f63c7e11846255d201
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-09-08 12:19:29 +0430

    JAcobify - functions final lesson

--- exercises/functions.py
+++ exercises/functions.py
@@ -111,3 +111,49 @@ def destination_setup(origin,destination,estimated_time,mode_of_transport="Car")
   print("It will take approximately",estimated_time,"hours")
 
 destination_setup("Mashhad","Vida",estimate)
+
+# ------------------------------------------------
+# Uncomment this when you reach the "Use the Force" section
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+c = 3*10**8
+
+# Write your code below:
+
+def f_to_c(f_temp) :
+  c_temp =( f_temp - 32 ) * 5/9
+  return c_temp
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
+
+
+def c_to_f(c_temp) :
+  f_temp = c_temp * (9/5) +32
+  return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+def get_force(mass,acceleration) :
+  return mass*acceleration
+
+train_force = get_force(train_mass,train_acceleration)
+print("The GE train supplies",train_force,"Newtons of force.")
+
+
+def get_energy(mass,c =3*10**8 ):
+  return mass*c**2
+
+bomb_energy= get_energy(bomb_mass)
+print("A 1kg bomb supplies",bomb_energy/10**12,"Terra Joules.")
+
+
+
+def get_work(mass,acceleration,distance):
+  return get_force(mass,acceleration)*distance
+
+train_work =  get_work(train_mass,train_acceleration,train_distance)
+print("The GE train does",train_work/1000,"Kilo Joules of work over",train_distance,"meters.")
