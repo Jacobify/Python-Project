9ef7e770e89512a3c0b77201269e862f0d0f90b4
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-09-11 14:24:16 +0430

    Yaghoobi - test boolean operator

---
+++ exercises/operator.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+
+# if a == 1 & b ==2:
+#   print("true")
+
+
+def always_false(num):
+  if num > 6 and num <-1:
+    return False
+  else:
+    return True
+# Uncomment these function calls to test your always_false function:
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
