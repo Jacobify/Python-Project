e0d52defa56be8c1ac00358149ad95a150ef785c
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-09-11 11:23:52 +0430

    Yaghoobi - jupyter practice

--- exercises/lists advanced.py
+++ exercises/lists advanced.py
@@ -29,6 +29,7 @@ print(list(zero_to_seven))
 # Your code below:
 
 range_five_three = range(5, 15, 3)
+print("list(range_five_three)")
 print(list(range_five_three))
 
 range_diff_five = range(0,40,5)
